2fb5caf9daa9640fcd4ba8de019bd78e6d6ff66a
punnybandnerd <tylerjpark@gmail.com>
2024-07-09 17:09:51 -0700

    Create practice.java

---
+++ practice.java
@@ -0,0 +1,12 @@
+public class practice {
+    public static void main(String []args){
+        double A = 16;
+        double B = 7;
+        double height = 8;
+        double extra = 0.5;
+
+        double areaOfTrapezpoid = A * B * height * extra;
+        System.out.println(areaOfTrapezpoid);
+    }
+
+}
